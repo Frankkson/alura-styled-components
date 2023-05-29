06afa1c74b4727d495f76e4b7b6b69d888a46562
Franklin <ffrancadesouza@gmail.com>
2023-05-29 09:55:22 -0300

    finalização da refatoração em StyledComponents

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -2,15 +2,31 @@ import React from "react";
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+const StyledContent = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`;
 
 const Container = () => {
   return (
-    <div className="container">
+    <StyledContainer>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <StyledContent>
         <Conta />
-      </section>
-    </div>
+      </StyledContent>
+    </StyledContainer>
   );
 };
 
